d3410a82dd37357126f91032d94730f0213b1461
Roshan84ya <45319711+Roshan84ya@users.noreply.github.com>
2020-10-13 09:05:29 -0700

    Add files via upload

--- competitive programming/number theory/gcd_calculaion_lcm.py
+++ competitive programming/number theory/gcd_calculaion_lcm.py
@@ -1,16 +1,27 @@
 """
-euclid theoram
-it calculates the gcd of a numeber in log2(min(a,b)) << min(a , b) 
+euclid throram it calculates the gcd( a, b) in log2(min(a,b)) time
+
 """
 
+#recursive
 def gcd(a , b):
 
     if (b==0):
         return a
     else:
-        return gcd(b,a%b);
+        return gcd(b,a%b)
+
+
+#iterative
+def gcdi(a , b):
+    while b:
+        a ,b = b , a%b
+    return a
     
 a, b = map(int, input().split())
 
 hcf = gcd(a,b)
 lcm = (a*b)/hcf
+hcf1 = gcdi(a,b)
+
+print(hcf  , hcf1)
